e7229958b2c5747f0decc8dbd792839d55be40c7
PragyaSukrity <paggy2712@gmail.com>
2024-04-27 16:26:24 +0530

    view engine

---
+++ view_engine/App.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+//express app
+const app = express();
+
+//listen for request
+app.listen(3000);
+
+//register  view engine
+app.set("view engine", "ejs");
+
+//respond to requests
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "ABC",
+      snippet: "Face the sun",
+    },
+    {
+      title: "XYZ",
+      snippet: "Face the sun  2",
+    },
+    {
+      title: "LMP",
+      snippet: "Face the sun  3",
+    },
+  ];
+  //render view
+  res.render("index", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
