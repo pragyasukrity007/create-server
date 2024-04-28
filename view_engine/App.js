fb0df21d39e80691fe3d14c71f9d1f2f04868b3c
PragyaSukrity <paggy2712@gmail.com>
2024-04-28 17:58:21 +0530

    middleware

--- view_engine/App.js
+++ view_engine/App.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 
 //express app
 const app = express();
 
 //listen for request
 app.listen(3000);
+
+//creating middleware
+app.use(morgan("dev"));
 
 //register  view engine
 app.set("view engine", "ejs");
